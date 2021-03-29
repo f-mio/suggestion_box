453ab58913d0862f05b55dd9456bedf6e1f5d283
f-mio <snowy.landscape.24.oct@gmail.com>
2021-03-29 21:09:18 +0900

    ER図の作成
    ・ER図作成
    ・suggestions tableの作成及びマイグレーション

--- db/migrate/20210328092048_create_suggestions.rb
+++ db/migrate/20210328092048_create_suggestions.rb
@@ -1,6 +1,14 @@
 class CreateSuggestions < ActiveRecord::Migration[6.0]
   def change
     create_table :suggestions do |t|
+      t.string     :title,       null: false
+      t.text       :issue,       null: false
+      t.text       :ideal,       null: false
+      t.references :category,    null: false
+      t.string     :target,      null: false
+      t.text       :effect,      null: false
+      t.references :user,        null: false
+      t.references :department,  null: false
 
       t.timestamps
     end

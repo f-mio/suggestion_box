67bffc63b262565df20cec3636d7b0b1b99f7307
f-mio <snowy.landscape.24.oct@gmail.com>
2021-03-29 22:04:33 +0900

    deviseインストール、ビュー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_28_092048) do
+ActiveRecord::Schema.define(version: 2021_03_29_125800) do
 
   create_table "suggestions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -26,6 +26,20 @@ ActiveRecord::Schema.define(version: 2021_03_28_092048) do
     t.index ["category_id"], name: "index_suggestions_on_category_id"
     t.index ["department_id"], name: "index_suggestions_on_department_id"
     t.index ["user_id"], name: "index_suggestions_on_user_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "corporate_no", default: "", null: false
+    t.string "name", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

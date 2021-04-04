6714702a2c4243d6d68b684d0fff034ca388ba41
f-mio <snowy.landscape.24.oct@gmail.com>
2021-04-04 20:16:03 +0900

    views/newの作成(約70％)

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,4 +1,14 @@
 class SuggestionsController < ApplicationController
   def index
   end
+
+  def new
+    @suggestion = Suggestion.new
+  end
+
+  def create
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "suggestions#index"
 
-  
+  resources :suggestions, only: [:index, :new, :create, :show]
+
 end

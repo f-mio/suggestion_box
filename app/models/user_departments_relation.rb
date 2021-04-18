2f78f7960cf83986693ceeb0ed2f43d7009f3428
f-mio <snowy.landscape.24.oct@gmail.com>
2021-04-18 19:51:32 +0900

    モデルuser_departments_relations.rbのバリデーション設定

--- app/models/user_departments_relation.rb
+++ app/models/user_departments_relation.rb
@@ -2,4 +2,5 @@ class UserDepartmentsRelation < ApplicationRecord
   belongs_to :user
   belongs_to :department
 
+  validates :is_manager, presense: true, inclution: {in: [true, false], message: "Bool値を入力して下さい"}
 end
